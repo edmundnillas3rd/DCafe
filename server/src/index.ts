9a6e4ffb75297a6ce9c4a29887aed2c9469a51a9
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-07-03 14:56:43 +0800

    Refactor app methods to main

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,10 +6,14 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+const main = async () => {
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+  });
 
-app.listen(port, function main() {
-  console.log(`Server is running at port: ${port}`);
-});
+  app.listen(port, function main() {
+    console.log(`Server is running at port: ${port}`);
+  });
+};
+
+main().catch((err) => console.error(err));
