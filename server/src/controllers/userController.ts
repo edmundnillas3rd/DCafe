a4278148f59d8a6c5971eee47604f2f61920da55
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-07-04 21:04:40 +0800

    Add function for adding new users

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,12 +1,22 @@
 import { Request, Response } from "express";
 import pool from "../utils/database";
 
-export function getUsers(req: Request, res: Response) {
-  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
-    if (error) {
-      throw error;
-    }
+export async function getUsers(req: Request, res: Response) {
+  // pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+  //   if (error) {
+  //     throw error;
+  //   }
 
-    res.status(200).json(results.rows);
-  });
+  //   res.status(200).json(results.rows);
+  // });
+
+  const results = await pool.query("SELECT * FROM users ORDER BY id ASC");
+  res.status(200).json(results.rows);
+}
+
+export async function addUser(req: Request, res: Response) {
+  const { email, firstName, lastName, password, profileUrl }: User = req.body;
+  const subscribers = 0;
+  const queryString = `INSERT INTO users VALUES (${email}, ${firstName}, ${lastName}, ${password}, ${profileUrl}, ${subscribers})`;
+  const results = await pool.query(queryString);
 }
