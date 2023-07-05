7da05243e499e9729a6d577e84e86bb7e4acbb93
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-07-05 09:25:04 +0800

    Add types for the corresponding tables

--- server/types.d.ts
+++ server/types.d.ts
@@ -1,7 +1,32 @@
 interface User {
+  id?: string;
   email: string;
   firstName: string;
   lastName: string;
   password: string;
   profileUrl: string;
+  subscribers: number;
+}
+
+interface Video {
+  id?: string;
+  name: string;
+  thumbnailUrl: string;
+  url: string;
+  uploadDate: string;
+  userID: string;
+}
+
+interface Library {
+  id?: string;
+  name: string;
+  displayState: string;
+}
+
+interface Comment {
+  id?: string;
+  description: string;
+  date: string;
+  videoID: string;
+  userID: string;
 }
