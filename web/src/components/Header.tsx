064c9454bcd1c76ef6856a2c3844631c90370b77
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-07-15 20:09:37 +0800

    Refactor components directory and Header component

---
+++ web/src/components/Header.tsx
@@ -0,0 +1,102 @@
+import { AiOutlineSearch } from "react-icons/ai";
+import { FiSettings } from "react-icons/fi";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
+
+export default function Headers() {
+  const iconProps = {
+    size: `16px`,
+  };
+
+  return (
+    <nav
+      className="d-flex navbar navbar-expand-lg bg-dark "
+      data-bs-theme="dark"
+    >
+      <div className="container-fluid">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="container">
+              <a className="d-flex navbar-brand" href="#">
+                <img src="/public/logo.png" alt="logo" width={30} height={30} />
+                DCafe
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex flex-fill p-2" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button
+              className="d-flex justify-content-center align-items-center btn btn-outline-success"
+              type="submit"
+            >
+              <AiOutlineSearch {...iconProps} />
+            </button>
+          </form>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item dropdown">
+              <a
+                className="d-flex justify-content-center align-items-center nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <CgProfile />
+              </a>
+              <ul className="dropdown-menu dropdown-menu-lg-end">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item dropdown">
+              <a
+                className="d-flex justify-content-center align-items-center nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <FiSettings {...iconProps} />
+              </a>
+              <ul className="dropdown-menu dropdown-menu-lg-end">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="d-flex justify-content-center align-items-center nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <IoIosNotificationsOutline />
+              </a>
+              <ul className="dropdown-menu dropdown-menu-lg-end">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
