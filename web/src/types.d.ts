afe566b5cea8f2f44a020deb7a9c528d046c49fe
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-07-23 15:16:35 +0800

    Add video type

--- web/src/types.d.ts
+++ web/src/types.d.ts
@@ -7,3 +7,17 @@ export type Props = {
   profileUrl?: string;
   size?: string;
 };
+
+export type Video = {
+  video_id: string;
+  video_name: string;
+  video_thumbnail_url: string;
+  video_url: string;
+  video_upload_date: string;
+  user_id: string;
+  video_likes: number;
+  video_dislikes: number;
+  video_views: number;
+  video_thumbnail_public_id: string;
+  video_public_id: string;
+};
