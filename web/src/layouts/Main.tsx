f6bafe9c319678d0d78974489ec252d1adcafbd5
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-07-23 15:18:34 +0800

    Refactor to remove fetching for videos

--- web/src/layouts/Main.tsx
+++ web/src/layouts/Main.tsx
@@ -1,21 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
-import { useEffect } from "react";
-
-export default function Home() {
-  const getVideos = async () => {
-    const response = await fetch(`${import.meta.env.VITE_BASE_URL as string}/v1/video`);
-    const data = await response.json() as Promise<any>;
-  };
-
-  useEffect(() => {
-    getVideos().catch((error) => console.error(error));
-  });
-
+export default function Main() {
   return (
-    <div>
+    <>
       <Header />
       <Outlet />
-    </div>
+    </>
   );
 }
