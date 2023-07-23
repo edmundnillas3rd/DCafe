5c151a43d0b793f1a0e3cd0da21dc4a8bf3b7767
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-07-23 15:17:16 +0800

    Add new routes for Home and Media Player

--- web/src/router/router.tsx
+++ web/src/router/router.tsx
@@ -1,11 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import Home from "../layouts/Main";
+import Layout from "../layouts/Main";
+import MediaPlayer from "../pages/MediaPlayer";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "video-watch/:videoID",
+        element: <MediaPlayer />,
+      },
+    ],
   },
 ]);
 
