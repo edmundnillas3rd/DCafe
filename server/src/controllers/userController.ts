18af70da6953bab28a6d26e4643380cc34ce81ae
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-07-24 20:35:29 +0800

    Add jwt signing before user login and checking of user authorization

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,15 +1,29 @@
 import { Request, Response, NextFunction } from "express";
-import pool from "../utils/database";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+import pool from "../utils/database";
+
+dotenv.config();
 
 // export async function getUsers(req: Request, res: Response) {
 //   const results = await pool.query("SELECT * FROM users ORDER BY id ASC");
 //   res.status(200).json(results.rows);
 // }
 
+interface IUser extends User {
+  user_id: string;
+}
+
 // POST
 export async function loginUser(req: Request, res: Response) {
-  res.status(200).json({ message: "Successfully login" });
+  const user: IUser = req.user as IUser;
+  const token = jwt.sign(
+    { id: user?.user_id as string },
+    process.env.JWT_SECRET as string
+  );
+  res.status(200).json({ message: "Successfully login", token });
 }
 
 export async function logoutUser(
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import { signupUser, loginUser, logoutUser } from "../controllers/userController";
-import { authenticate } from "../utils/passportjsConfig";
+import { authenticate, authorize } from "../utils/passportjsConfig";
 
 const router = express.Router();
 
 // POST
 router.post("/signup", signupUser);
 router.post("/login", authenticate, loginUser);
-router.post("/logout", logoutUser);
+router.post("/logout", authorize, logoutUser);
 
 export default router;
