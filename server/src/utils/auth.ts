022b8975c49223e2d355555489e2e62335ea96f2
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-07-24 20:34:28 +0800

    Add configuration for passport-jwt

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -1,9 +1,37 @@
 import { Strategy as LocalStrategy } from "passport-local";
+import {
+  Strategy as JwtStrategy,
+  ExtractJwt,
+  type StrategyOptions,
+} from "passport-jwt";
 import bcrypt from "bcrypt";
 
 import pool from "./database";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const pjs = (passport: any) => {
+  const options: StrategyOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET,
+  };
+  
+  passport.use(
+    new JwtStrategy(options, async function verify(jwt_payload, done) {
+      const results = await pool.query(
+        "SELECT * FROM users WHERE user_id = $1",
+        [jwt_payload.id]
+      );
+
+      if (results.rows.length) {
+        return done(null, results.rows[0]);
+      } else {
+        return done(null, false);
+      }
+    })
+  );
+
   passport.use(
     new LocalStrategy(async function verify(
       username: string,
@@ -11,7 +39,7 @@ const pjs = (passport: any) => {
       cb: any
     ) {
       const results = await pool.query(
-        `SELECT * FROM users WHERE username = $1`,
+        "SELECT * FROM users WHERE username = $1",
         [username]
       );
 
--- server/src/utils/passportjsConfig.ts
+++ server/src/utils/passportjsConfig.ts
@@ -1,3 +1,4 @@
 import passport from "passport";
 
 export const authenticate = passport.authenticate("local", { session: false });
+export const authorize = passport.authenticate("jwt", { session: false });
