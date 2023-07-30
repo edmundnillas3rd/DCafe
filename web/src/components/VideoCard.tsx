ec93be89fa91c4494523f392d92fc871776765d6
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-07-30 19:25:21 +0800

    Add styling for video cards

---
+++ web/src/components/VideoCard.tsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import { Video } from "../types";
+
+export default function VideoCard({
+  video_name,
+  video_thumbnail_url,
+  video_url,
+  video_id,
+  video_upload_date,
+  user_id,
+  video_likes,
+  video_dislikes,
+  video_views,
+  video_thumbnail_public_id,
+  video_public_id,
+}: Video) {
+  return (
+    <Link
+      to={video_url}
+      className="d-flex w-25 flex-wrap flex-column gap-2 text-reset link-underline link-underline-opacity-0"
+      style={{
+        width: `300px`,
+      }}
+    >
+      <img className="w-100 h-100" src={video_thumbnail_url} alt={video_name} />
+      <div className="w-100 h-100 d-flex flex-column text-break">
+        <p className="fw-bold m-0">{video_name}</p>
+        <p className="m-0">{user_id}</p>
+      </div>
+
+      <div className="w-100 h-100 d-flex gap-1">
+        <p className="m-0">
+          {video_views} {video_views > 0 ? "Views" : "View"}
+        </p>
+        | <p>{video_upload_date}</p>
+      </div>
+    </Link>
+  );
+}
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { type Video } from "../types";
+import VideoCard from "../components/VideoCard";
 
 export default function Home() {
   const [videos, setVideos] = useState<Video[]>([]);
@@ -19,17 +20,24 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
-      {videos.length !== 0 &&
-        videos.map((v: Video) => (
-          <Link
-            key={v.video_id}
-            className="link-info"
-            to={`video-watch/${v.video_id}`}
-          >
-            {v.video_name}
-          </Link>
-        ))}
+    <div className="container">
+        {videos.length !== 0 &&
+          videos.map((v: Video) => (
+            <VideoCard
+              key={v.video_id}
+              video_name={v.video_name}
+              video_thumbnail_url={v.video_thumbnail_url}
+              video_id={v.video_id}
+              video_url={v.video_url}
+              video_upload_date={v.video_upload_date}
+              user_id={v.user_id}
+              video_likes={v.video_likes}
+              video_dislikes={v.video_dislikes}
+              video_views={v.video_views}
+              video_thumbnail_public_id={v.video_thumbnail_public_id}
+              video_public_id={v.video_public_id}
+            />
+          ))}
     </div>
   );
 }
